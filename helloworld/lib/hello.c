94d501de7efff478d73eb8adb8de9577d2ce6662
liuhuanyun <406125676@qq.com>
2014-09-24 18:14:25 +0800

    cmake examples
    cmake examples for test

---
+++ helloworld/lib/hello.c
@@ -0,0 +1,9 @@
+#include "hello.h"
+#include <stdio.h>
+#include <windows.h>
+
+void hellofunc()
+{
+	printf("Hello world\n");
+	system("pause");
+}
---
+++ helloworld/src/main.c
@@ -0,0 +1,8 @@
+// main.c
+#include "hello.h"
+
+int main()
+{
+	hellofunc();
+	return 0;
+}
